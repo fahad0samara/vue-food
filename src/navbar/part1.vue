6e81941d9430e2db38f9e5aec58af3afe40be916
fahad0samara <fah8ad@outlook.com>
2022-03-23 09:36:41 +0200

    add img

--- src/navbar/part1.vue
+++ src/navbar/part1.vue
@@ -1,3 +1,253 @@
 <template>
+<section>
+  <div class="skewed bg-yellow-400"></div>
+</section>
+<div class="body">
+    <nav class="menu-container">
+  <!-- burger menu -->
+  <input type="checkbox" aria-label="Toggle menu" />
+  <span></span>
+  <span></span>
+  <span></span>
 
+  <!-- logo -->
+  <a href="" class="menu-logo">
+<h1 class=" text-2xl"> foodLink</h1>
+    
+  </a>
+
+  <!-- menu items -->
+  <div class="menu">
+    <ul>
+      <li>
+        <a href="#home">
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="#pricing">
+          Pricing
+        </a>
+      </li>
+      <li>
+        <a href="#blog">
+          about us
+        </a>
+      </li>
+      <li>
+        <a href="#docs">
+          content us
+        </a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <a href="#signup">
+          Sign-up
+        </a>
+      </li>
+      <li>
+        <a href="#login">
+          Login
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</div>
+<div class=" grid grid-cols-2 absolute  ">
+<div> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque consectetur ipsam reiciendis quasi voluptates. Est quam natus vero odit culpa tenetur vitae sequi delectus quod fugit, ex provident labore nobis.</div>
+<div class="img ">
+<img src="../assets/0.jpeg" alt="" class=" rounded-full">
+</div>
+</div>
 </template>
+<style scoped>
+.img {
+    height: 500px;
+    width: 500px;}
+.skewed {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+ 
+  z-index: 0;
+  transform: skewY(35deg);
+  transform-origin: top right;
+}
+
+.menu-container {
+  position: relative;
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+
+  color: #0b0b0b;
+  padding: 20px;
+  z-index: 1;
+  -webkit-user-select: none;
+  user-select: none;
+  box-sizing: border-box;
+}
+
+.menu-logo {
+  line-height: 0;
+  margin: 0 20px;
+}
+
+.menu-logo img {
+  max-height: 40px;
+  max-width: 100px;
+  flex-shrink: 0;
+}
+
+.menu-container a {
+  text-decoration: none;
+  color: #edede6;
+  transition: color 0.3s ease;
+}
+
+.menu-container a:hover {
+  color: #0aefcb;
+}
+
+.menu-container input {
+  display: block;
+  width: 35px;
+  height: 25px;
+  margin: 0;
+  position: absolute;
+  cursor: pointer;
+  opacity: 0; /* hide this */
+  z-index: 2; /* and place it over the hamburger */
+  -webkit-touch-callout: none;
+}
+
+/* Burger menu */
+.menu-container span {
+  display: block;
+  width: 33px;
+  height: 4px;
+  margin-bottom: 5px;
+  position: relative;
+  background: #f00505;
+  border-radius: 3px;
+  z-index: 1;
+  transform-origin: 4px 0px;
+  transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
+              background 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
+              opacity 0.55s ease;
+}
+
+.menu-container span:first-child {
+  transform-origin: 0% 0%;
+}
+
+.menu-container span:nth-child(3) {
+  transform-origin: 0% 100%;
+}
+
+.menu-container input:checked ~ span {
+  opacity: 1;
+  transform: rotate(45deg) translate(3px,-1px);
+  background: #edede6;
+}
+
+.menu-container input:checked ~ span:nth-child(4) {
+  opacity: 0;
+  transform: rotate(0deg) scale(0.2, 0.2);
+}
+
+.menu-container input:checked ~ span:nth-child(3) {
+  transform: rotate(-45deg) translate(-5px,11px);
+}
+
+.menu ul {
+  list-style: none;
+}
+
+.menu li {
+  padding: 10px 0;
+  font-size: 22px;
+}
+
+/* mobile styles */
+@media only screen and (max-width: 830px) { 
+  .menu-container {
+    flex-direction: column;
+    align-items: flex-end;
+  }
+  
+  .menu-logo {
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  .menu-logo img {
+    max-height: 30px;
+  }
+
+  .menu {
+    position: absolute;
+    box-sizing: border-box;
+    width: 300px;
+ 
+    right: -300px;
+    top: 0;
+    margin: -20px;
+    padding: 75px 50px 50px;
+    background: #e40505;
+    -webkit-font-smoothing: antialiased;
+    /* to stop flickering of text in safari */
+    transform-origin: 0% 0%;
+    transform: translateX(0%);
+    transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0);
+  }
+
+  .menu-container input:checked ~ .menu {
+    transform: translateX(-100%);
+  }
+}
+
+/* desktop styles */
+@media only screen and (min-width: 831px) { 
+  .menu-container {
+    width: 100%;
+  }
+
+  .menu-container a {
+    color: #0b0b0b;
+  }
+
+  .menu-container input {
+    display: none;
+  }
+
+  /* Burger menu */
+  .menu-container span {
+    display: none;
+  }
+
+  .menu {
+    position: relative;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .menu ul {
+    display: flex;
+    padding: 0;
+  }
+
+  .menu li {
+    padding: 0 20px;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,13 @@
 <script setup>
+// import part0 from "../navbar/part0.vue";
 import part1 from "../navbar/part1.vue";
-
 
 </script>
 
 <template>
   <main>
   
+<!-- <part0></part0>  -->
 <part1></part1>
   </main>
 </template>
