a0da5dcce11fe15d77763109047894b1e05d7a31
fahad0samara <fah8ad@outlook.com>
2022-03-24 17:30:31 +0200

    add the section card

--- src/page/hero3.vue
+++ src/page/hero3.vue
@@ -1,15 +1,14 @@
 <template>
     <div class="div1 ">
-    <div class="flex flex-col justify-center items-center m-24 space-y-3">
+    <div class=" bg-yellow-400 flex flex-col justify-center items-center m-24 space-y-3">
     <h1 class=" text-4xl text-yellow-400 font-black ">Work</h1>
     <h1 class=" text-6xl font-extrabold">How It Works</h1>
     <h2 class=" text-3xl">It's through mistakes that you actually can grow you get<br> rid of
     
 everything that is not essential to makihave to get bad</h2>
 </div>
-d
 <div class=" grid grid-cols-3  gap-32 ">
-<div class=" div2 flex flex-col justify-center items-center space-y-3 ml-4" > 
+<div class="  div2 flex flex-col justify-center items-center space-y-3 ml-4" > 
 <span><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class=" text-yellow-400 iconify iconify--ic" width="50" height="50" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M16 14V6c0-1.76 2.24-4 5-4v16.17l-2-2V14h-3zm4.49 9.31L10.02 12.85c-.33.09-.66.15-1.02.15v9H7v-9c-2.21 0-4-1.79-4-4V5.83L.69 3.51L2.1 2.1l19.8 19.8l-1.41 1.41zM6.17 9L5 7.83V9h1.17zM9 2H7v2.17l2 2V2zm4 7V2h-2v6.17l1.85 1.85c.09-.33.15-.66.15-1.02z"></path></svg></span>
 
 <h1 class=" text-4xl font-extrabold">Pick Meals </h1>
@@ -27,13 +26,162 @@ options.</h2>
 <h2 class="text-xl font-extrabold">Our team of chefs do the prep
 work no more chopping, measuring, or sink full of dishes!</h2>
 </div>
-<div class="  flex flex-col justify-center items-center space-y-3"><span><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class=" text-yellow-400 iconify iconify--carbon" width="60" height="60" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M4 16h12v2H4zm-2-5h10v2H2z"></path><path fill="currentColor" d="m29.919 16.606l-3-7A.999.999 0 0 0 26 9h-3V7a1 1 0 0 0-1-1H6v2h15v12.556A3.992 3.992 0 0 0 19.142 23h-6.284a4 4 0 1 0 0 2h6.284a3.98 3.98 0 0 0 7.716 0H29a1 1 0 0 0 1-1v-7a.997.997 0 0 0-.081-.394ZM9 26a2 2 0 1 1 2-2a2.002 2.002 0 0 1-2 2Zm14-15h2.34l2.144 5H23Zm0 15a2 2 0 1 1 2-2a2.002 2.002 0 0 1-2 2Zm5-3h-1.142A3.995 3.995 0 0 0 23 20v-2h5Z"></path></svg></span>
+<div class=" div2 flex flex-col justify-center items-center space-y-3"><span><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class=" text-yellow-400 iconify iconify--carbon" width="60" height="60" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M4 16h12v2H4zm-2-5h10v2H2z"></path><path fill="currentColor" d="m29.919 16.606l-3-7A.999.999 0 0 0 26 9h-3V7a1 1 0 0 0-1-1H6v2h15v12.556A3.992 3.992 0 0 0 19.142 23h-6.284a4 4 0 1 0 0 2h6.284a3.98 3.98 0 0 0 7.716 0H29a1 1 0 0 0 1-1v-7a.997.997 0 0 0-.081-.394ZM9 26a2 2 0 1 1 2-2a2.002 2.002 0 0 1-2 2Zm14-15h2.34l2.144 5H23Zm0 15a2 2 0 1 1 2-2a2.002 2.002 0 0 1-2 2Zm5-3h-1.142A3.995 3.995 0 0 0 23 20v-2h5Z"></path></svg></span>
 
 <h1 class="text-4xl font-extrabold" >Fast Deliveries</h1>
 
 <h2 class=" text-xl font-extrabold" >Your freshly prepped 15-min dinner kits arrive on your doorstep in a
 refrigerated box.</h2></div>
 </div>  
+<div class="  rounded-b-2xl   flex flex-col justify-center items-center m-24 space-y-3">
+    <h1 class=" text-4xl  text-yellow-400 font-black ">Menu</h1>
+    <h1 class=" text-6xl font-extrabold">Explore Our Best Menu</h1>
+    <h2 class=" text-2xl font-medium ">It's through mistakes that you actually <br> can grow you get rid of
+everything that <br> is not essential to makinave to get bad.</h2>
+</div>
+<div class=" grid grid-cols-3  ">
+<div class="flex justify-center rounded-tl-full rounded-tr-3xl">
+  <div class="rounded-lg shadow-lg bg-white max-w-sm">
+    <a href="#!">
+      <img class="rounded-t-lg" src="../assets/4.jpg" alt=""/>
+    </a>
+    <div class="p-6">
+    <div class=" flex justify-between">
+      <h5 class="text-gray-900 text-4xl font-medium mb-2">Egg Muffen</h5>
+ <h2 class=" text-4xl font-black">12$</h2>
+ </div>
+      <p class="  text-yellow-800 mb-4 text-2xl  font-bold ">
+      Served with french fries + drink Choice of: Coke, Fanta, Sprite, Upgrade to large fries, Add whopper patty, Add Tender crisp patty and more...
+
+
+      </p>
+      <div class="flex  justify-end ">
+<button >
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify  bg-yellow-400   text-neutral-50 rounded-tl-full text-center iconify--carbon" width="200" height="70" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M17 15V8h-2v7H8v2h7v7h2v-7h7v-2z"></path></svg>
+      </button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="flex justify-center rounded-tl-full rounded-tr-3xl">
+  <div class="rounded-lg shadow-lg bg-white max-w-sm">
+    <a href="#!">
+      <img class="rounded-t-lg" src="../assets/6.jpg" alt=""/>
+    </a>
+    <div class="p-6">
+    <div class=" flex justify-between">
+      <h5 class="text-gray-900 text-4xl font-medium mb-2">Egg Muffen</h5>
+ <h2 class=" text-4xl font-black">14$</h2>
+ </div>
+      <p class="  text-yellow-800 mb-4 text-2xl  font-bold ">
+      Served with french fries + drink Choice of: Coke, Fanta, Sprite, Upgrade to large fries, Add whopper patty, Add Tender crisp patty and more...
+
+
+      </p>
+      <div class="flex  justify-end ">
+<button >
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify  bg-yellow-400   text-neutral-50 rounded-tl-full text-center iconify--carbon" width="200" height="70" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M17 15V8h-2v7H8v2h7v7h2v-7h7v-2z"></path></svg>
+      </button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="flex justify-center rounded-tl-full rounded-tr-3xl">
+  <div class="rounded-lg shadow-lg bg-white max-w-sm">
+    <a href="#!">
+      <img class="rounded-t-lg" src="../assets/7.jpg" alt=""/>
+    </a>
+    <div class="p-6">
+    <div class=" flex justify-between">
+      <h5 class="text-gray-900 text-4xl font-medium mb-2">Egg Muffen</h5>
+ <h2 class=" text-4xl font-black">20$</h2>
+ </div>
+      <p class="  text-yellow-800 mb-4 text-2xl  font-bold ">
+      Served with french fries + drink Choice of: Coke, Fanta, Sprite, Upgrade to large fries, Add whopper patty, Add Tender crisp patty and more...
+
+
+      </p>
+      <div class="flex  justify-end ">
+<button >
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify  bg-yellow-400   text-neutral-50 rounded-tl-full text-center iconify--carbon" width="200" height="70" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M17 15V8h-2v7H8v2h7v7h2v-7h7v-2z"></path></svg>
+      </button>
+      </div>
+      
+    </div>
+  </div>
+</div>
+<div class="flex justify-center rounded-tl-full rounded-tr-3xl">
+  <div class="rounded-lg shadow-lg bg-white max-w-sm">
+    <a href="#!">
+      <img class="rounded-t-lg" src="../assets/4.jpg" alt=""/>
+    </a>
+    <div class="p-6">
+    <div class=" flex justify-between">
+      <h5 class="text-gray-900 text-4xl font-medium mb-2">Egg Muffen</h5>
+ <h2 class=" text-4xl font-black">18$</h2>
+ </div>
+      <p class="  text-yellow-800 mb-4 text-2xl  font-bold ">
+      Served with french fries + drink Choice of: Coke, Fanta, Sprite, Upgrade to large fries, Add whopper patty, Add Tender crisp patty and more...
+
+
+      </p>
+      <div class="flex  justify-end ">
+<button >
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify  bg-yellow-400   text-neutral-50 rounded-tl-full text-center iconify--carbon" width="200" height="70" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M17 15V8h-2v7H8v2h7v7h2v-7h7v-2z"></path></svg>
+      </button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="flex justify-center rounded-tl-full rounded-tr-3xl">
+  <div class="rounded-lg shadow-lg bg-white max-w-sm">
+    <a href="#!">
+      <img class="rounded-t-lg " src="../assets/11.png" alt=""/>
+    </a>
+    <div class="p-6">
+    <div class=" flex justify-between">
+      <h5 class="text-gray-900 text-4xl font-medium mb-2">Egg Muffen</h5>
+ <h2 class=" text-4xl font-black">5$</h2>
+ </div>
+      <p class="  text-yellow-800 mb-4 text-2xl  font-bold ">
+      Served with french fries + drink Choice of: Coke, Fanta, Sprite, Upgrade to large fries, Add whopper patty, Add Tender crisp patty and more...
+
+
+      </p>
+      <div class="flex  justify-end ">
+<button >
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify  bg-yellow-400   text-neutral-50 rounded-tl-full text-center iconify--carbon" width="200" height="70" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M17 15V8h-2v7H8v2h7v7h2v-7h7v-2z"></path></svg>
+      </button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="flex justify-center rounded-tl-full rounded-tr-3xl ">
+  <div class="rounded-lg shadow-lg bg-white max-w-sm">
+    <a href="#!">
+      <img class="rounded-t-lg" src="../assets/8.jpg" alt=""/>
+    </a>
+    <div class="p-6">
+    <div class=" flex justify-between">
+      <h5 class="text-gray-900 text-4xl font-medium mb-2">Egg Muffen</h5>
+ <h2 class=" text-4xl font-black">19$</h2>
+ </div>
+      <p class="  text-yellow-800 mb-4 text-2xl  font-bold ">
+      Served with french fries + drink Choice of: Coke, Fanta, Sprite, Upgrade to large fries, Add whopper patty, Add Tender crisp patty and more...
+
+
+      </p>
+      <div class="flex  justify-end ">
+<button >
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify  bg-yellow-400   text-neutral-50 rounded-tl-full text-center iconify--carbon" width="200" height="70" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M17 15V8h-2v7H8v2h7v7h2v-7h7v-2z"></path></svg>
+      </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+</div>
     </div>
 
 </template>
@@ -44,12 +192,13 @@ position: relative;
 
 }
 .div2{
-box-shadow:
-  99.2px 112.9px 22.2px rgba(0, 0, 0, 0.03),
-  108.5px 123.5px 37.1px rgba(0, 0, 0, 0.043),
-  167px 190px 80px rgba(0, 0, 0, 0.07)
+
+background: linear-gradient(-145deg, );
+box-shadow: 
+             -50px 50px 100px #7c7b7b;
 
 ;}
+
 
 
 
